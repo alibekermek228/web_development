19956d807eab86007c779452fd74985103643231
alibekermek228 <al_yermekuly@kbtu.kz>
2026-03-05 05:41:33 +0500

    online stor

---
+++ src/app/components/product-card/product-card.component.ts
@@ -0,0 +1,40 @@
+﻿import { CurrencyPipe, DecimalPipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../models/product.model';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [CurrencyPipe, DecimalPipe],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.css'
+})
+export class ProductCardComponent {
+  @Input({ required: true }) product!: Product;
+
+  stars = [1, 2, 3, 4, 5];
+  selectedImage = '';
+
+  get currentImage(): string {
+    return this.selectedImage || this.product.image;
+  }
+
+  isFilledStar(star: number): boolean {
+    return this.product.rating >= star;
+  }
+
+  selectImage(image: string): void {
+    this.selectedImage = image;
+  }
+
+  buildWhatsappUrl(): string {
+    const text = encodeURIComponent(`Check out this product: ${this.product.link}`);
+    return `https://wa.me/?text=${text}`;
+  }
+
+  buildTelegramUrl(): string {
+    const url = encodeURIComponent(this.product.link);
+    const text = encodeURIComponent(this.product.name);
+    return `https://t.me/share/url?url=${url}&text=${text}`;
+  }
+}
---
+++ src/app/data/products.ts
@@ -0,0 +1,164 @@
+﻿import { Product } from '../models/product.model';
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: 'Apple iPhone 15 128Gb NanoSIM+eSIM черный',
+    description:
+      'Новый iPhone 15 с ярким экраном, хорошей камерой и быстрым откликом в повседневных задачах.',
+    price: 429990,
+    rating: 4.8,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/h65/h81/86275143532574.jpg?format=gallery-large',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h65/h81/86275143532574.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h1d/hfc/86303745998878.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h81/h37/86303335186462.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h7a/h3f/86303338397726.jpg?format=gallery-large'
+    ],
+    link: 'https://kaspi.kz/shop/p/apple-iphone-15-128gb-nanosim-esim-chernyi-113137790/'
+  },
+  {
+    id: 2,
+    name: 'Apple iPhone 13 128Gb NanoSIM+eSIM черный',
+    description:
+      'Проверенный iPhone 13, который до сих пор отлично работает для фото, соцсетей и учебы.',
+    price: 319990,
+    rating: 4.7,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/h32/h70/84378448199710.jpg?format=gallery-large',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h32/h70/84378448199710.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h3d/h8e/64208874405918.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/hfe/h17/64208876634142.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/hc9/h90/64209083007006.jpg?format=gallery-large'
+    ],
+    link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-nanosim-esim-chernyi-102298404/'
+  },
+  {
+    id: 3,
+    name: 'Xiaomi Redmi 13C 8 ГБ/256 ГБ черный',
+    description:
+      'Бюджетный смартфон с большим объемом памяти и емкой батареей на каждый день.',
+    price: 69990,
+    rating: 4.5,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/h67/h01/84526692565022.png?format=gallery-large',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h67/h01/84526692565022.png?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/hf4/h4b/84526902771742.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h1b/h77/84526902706206.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h94/heb/84957845651486.jpg?format=gallery-large'
+    ],
+    link: 'https://kaspi.kz/shop/p/xiaomi-redmi-13c-8-gb-256-gb-chernyi-114695323/'
+  },
+  {
+    id: 4,
+    name: 'Xiaomi Redmi Note 13 4G NFC 8 ГБ/256 ГБ черный',
+    description:
+      'Сбалансированная модель Redmi Note с NFC, хорошим экраном и нормальной производительностью.',
+    price: 104990,
+    rating: 4.6,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/h66/h38/84949953773598.jpg?format=gallery-large',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h66/h38/84949953773598.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h4b/hea/84951023648798.png?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h60/hab/84949954035742.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/hf0/hff/84951023681566.jpg?format=gallery-large'
+    ],
+    link: 'https://kaspi.kz/shop/p/xiaomi-redmi-note-13-4g-nfc-8-gb-256-gb-chernyi-115994720/'
+  },
+  {
+    id: 5,
+    name: 'Samsung Galaxy A55 5G 8 ГБ/128 ГБ темно-синий',
+    description:
+      'Galaxy A55 с качественным AMOLED-дисплеем и аккуратным дизайном в металлической рамке.',
+    price: 179990,
+    rating: 4.7,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/h03/h20/85428948598814.png?format=gallery-large',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h03/h20/85428948598814.png?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/hc5/h27/85429036056606.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h6f/ha2/85428544471070.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h42/h7d/85428948729886.png?format=gallery-large'
+    ],
+    link: 'https://kaspi.kz/shop/p/samsung-galaxy-a55-5g-8-gb-128-gb-temno-sinii-117420239/'
+  },
+  {
+    id: 6,
+    name: 'Apple AirPods 3 with Lightning Charging Case белый',
+    description:
+      'AirPods 3 с удобной посадкой и чистым звуком, особенно хорошо работают с iPhone.',
+    price: 94990,
+    rating: 4.8,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/he8/h47/64362670358558.jpg?format=gallery-large',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/he8/h47/64362670358558.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h63/h66/64362740416542.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h30/ha4/64362737860638.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h31/hd7/64362668556318.jpg?format=gallery-large'
+    ],
+    link: 'https://kaspi.kz/shop/p/naushniki-apple-airpods-3-with-lightning-charging-case-belyi-106667987/'
+  },
+  {
+    id: 7,
+    name: 'JBL Tune 520BT черный',
+    description:
+      'Легкие Bluetooth-наушники JBL с плотным басом и долгой работой без подзарядки.',
+    price: 29990,
+    rating: 4.6,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/hcf/h93/80522158080030.jpg?format=gallery-large',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/hcf/h93/80522158080030.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h3e/h87/80522158211102.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/hc2/hdf/81348676812830.png?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/hef/h04/80522158145566.jpg?format=gallery-large'
+    ],
+    link: 'https://kaspi.kz/shop/p/naushniki-jbl-tune-520bt-chernyi-110103473/'
+  },
+  {
+    id: 8,
+    name: 'Lenovo IdeaPad Slim 3 15.6" / 16 Гб / SSD 512 Гб / Без ОС',
+    description:
+      'Практичный ноутбук Lenovo для учебы и офиса: 16 ГБ ОЗУ и быстрый SSD на 512 ГБ.',
+    price: 239990,
+    rating: 4.4,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/h86/ha1/85255950696478.png?format=gallery-large',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h86/ha1/85255950696478.png?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h58/h15/85255950762014.png?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/hda/hd5/85255950630942.png?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h38/h77/85255950827550.png?format=gallery-large'
+    ],
+    link: 'https://kaspi.kz/shop/p/lenovo-ideapad-slim-3-15-6-16-gb-ssd-512-gb-bez-os-15abr8-82xm00cgrk-116906372/'
+  },
+  {
+    id: 9,
+    name: 'ASUS Vivobook 15 X1504ZA-BQ092W 15.6" / 8 Гб / SSD 512 Гб / Win 11',
+    description:
+      'Универсальный Vivobook 15 для повседневной работы, браузера и учебных программ.',
+    price: 259990,
+    rating: 4.5,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/h34/he0/86722346942494.png?format=gallery-large',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/h34/he0/86722346942494.png?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h12/h9e/86722346745886.png?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h5a/h0c/86722346876958.png?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h32/h0f/86722346680350.png?format=gallery-large'
+    ],
+    link: 'https://kaspi.kz/shop/p/asus-vivobook-15-x1504za-bq092w-15-6-8-gb-ssd-512-gb-win-11-90nb1021-m01zd0-122144446/'
+  },
+  {
+    id: 10,
+    name: 'Xiaomi Mi TV A Pro 43 L43M8-A2ME 109 см черный',
+    description:
+      '4K-телевизор Xiaomi на 43 дюйма с современным Smart TV и сочной картинкой.',
+    price: 159990,
+    rating: 4.6,
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/hc9/hd1/85806649737246.jpg?format=gallery-large',
+    images: [
+      'https://resources.cdn-kaspi.kz/img/m/p/hc9/hd1/85806649737246.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h11/h40/85806649868318.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/h68/h53/85806649802782.jpg?format=gallery-large',
+      'https://resources.cdn-kaspi.kz/img/m/p/hae/hf0/85806649671710.jpg?format=gallery-large'
+    ],
+    link: 'https://kaspi.kz/shop/p/xiaomi-mi-tv-a-pro-43-l43m8-a2me-109-sm-chernyi-118632729/'
+  }
+];
