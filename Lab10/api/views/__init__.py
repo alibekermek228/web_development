d9686dedb9c88626b492c01bd39724f08d7b675f
alibekermek228 <al_yermekuly@kbtu.kz>
2026-04-09 09:30:07 +0500

    Add files via upload

---
+++ Lab10/api/views/__init__.py
@@ -0,0 +1,15 @@
+from .generics import (
+    CategoryDetailAPIView,
+    CategoryListAPIView,
+    CategoryProductsAPIView,
+    ProductDetailAPIView,
+    ProductListAPIView,
+)
+
+__all__ = [
+    'CategoryListAPIView',
+    'CategoryDetailAPIView',
+    'CategoryProductsAPIView',
+    'ProductListAPIView',
+    'ProductDetailAPIView',
+]
